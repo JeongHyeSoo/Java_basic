627500d3bcdb251e02e57b7a8978296465794b23
JeongHyeSoo <nailonly@naver.com>
2022-08-07 00:51:28 +0900

    ArrayList ArrayList_sizeUpIfFull함수 도입

--- src/main/java/CH_30_ArrayList/ArrayList_add함수.java
+++ src/main/java/CH_30_ArrayList/ArrayList_add함수.java
@@ -1,2 +1,38 @@
-package CH_30_ArrayList;public class ArrayList_add함수 {
+package CH_30_ArrayList;
+
+public class ArrayList_add함수 {	public static void main(String[] args) {
+    ArrayList ar = new ArrayList();
+    ar.add(100);
+    ar.add(200);
+    ar.add(300);
+
+    int value = ar.get(0);
+    System.out.println(value);
+    // 출력 : 100
+
+    value = ar.get(1);
+    System.out.println(value);
+    // 출력 : 200
+
+    value = ar.get(2);
+    System.out.println(value);
+    // 출력 : 300
+}
+}
+
+class ArrayList {
+    int[] datas;
+    int lastIndex;
+
+    ArrayList(){
+        datas = new int[3];
+        lastIndex = -1;
+    }
+    void add(int value) {
+        lastIndex++;
+        datas[lastIndex] = value;
+    }
+    int get(int index){
+        return datas[index];
+    }
 }
